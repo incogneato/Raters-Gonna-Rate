4dc69af89cde3a3bd166eb2e73d419948520ed99
Michael Wiggins <wigginsmichaelc@gmail.com>
2012-12-12 01:23:47 -0800

    Add before filter to business model to set rating to zero.

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,9 @@
 class Business < ActiveRecord::Base
-	attr_accessible :name, :location, :review
+	attr_accessible :name, :location, :review, :rating
 	validates_presence_of :name, :location
+
+	private
+	def set_rating_to_zero
+		self.rating = 0
+	end
 end
