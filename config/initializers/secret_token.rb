8f351f68affff6aa32b6e238474cea468ee1b20b
Michael Wiggins <wigginsmichaelc@gmail.com>
2012-12-11 19:54:29 -0800

    Commit #1, baby.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ratersgonnarate::Application.config.secret_token = 'dd212511ada677c9465bc46fe31b66c9c46ce6c70b2707f18d0d6df8bf67cb8dd7bbd6554db2c11650a12934984e23819d65d553b9d22118d804d9ac6227d1c2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ratersgonnarate::Application.config.session_store :cookie_store, key: '_ratersgonnarate_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ratersgonnarate::Application.config.session_store :active_record_store
