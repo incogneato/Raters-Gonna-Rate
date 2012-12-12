3e955a089aba7dfeffc92cee3c109b6f662b9b3c
Michael Wiggins <wigginsmichaelc@gmail.com>
2012-12-11 22:12:31 -0800

    Update BusinessesController to account for, and add, views: new, show, edit & partial: _form

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.9'
 gem 'devise'
 gem 'jquery-rails'
+gem 'thin'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -3,4 +3,40 @@ class BusinessesController < ApplicationController
 	def index
 		@businesses = Business.all		
 	end
+
+	def new
+		@business = Business.new
+	end
+
+	def edit
+		@business = Business.find(params[:id])
+	end
+
+	def create
+		@business = Business.create(params[:business])
+			if @business.save
+				redirect_to @business, notice: "Business successfully added."
+			else
+				redirect_to :root, flash: "Apologies, we could not add your business. Our bad."
+			end
+	end
+
+	def show
+		@business = Business.find(params[:id])
+	end
+
+	def update
+		@business = Business.find(params[:id])
+		if @business.update_attributes(params[:business])
+			redirect_to @business, notice: "Business successfully updated."
+		else
+			redirect_to :back, flash: "Apologies, we could not update your business."
+		end
+	end
+
+	def destroy
+		@business = Business.find(params[:id])
+		@business.destroy
+		redirect_to root_url, notice: "Business successfully deleted."
+	end
 end
