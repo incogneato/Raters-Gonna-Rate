2a31c9da84110561b4a65ad61f9682aecca2f485
Michael Wiggins <wigginsmichaelc@gmail.com>
2012-12-12 01:21:43 -0800

    Add rating column to businesses table.

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,4 @@
 class Business < ActiveRecord::Base
-	attr_accessible :name, :location
+	attr_accessible :name, :location, :review
 	validates_presence_of :name, :location
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121212085150) do
+ActiveRecord::Schema.define(:version => 20121212092053) do
 
   create_table "businesses", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20121212085150) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.text     "review"
+    t.integer  "rating"
   end
 
 end
