0e1c1a7b6738243b3bf3e96e3a5d72220cbe5c83
Michael Wiggins <wigginsmichaelc@gmail.com>
2012-12-12 22:47:06 -0800

    Update form, tweak css, add scope.

--- app/models/business.rb
+++ app/models/business.rb
@@ -2,6 +2,7 @@ class Business < ActiveRecord::Base
 	attr_accessible :name, :location, :review, :rating
 	before_create :set_rating_to_zero
 	validates_presence_of :name, :location
+	default_scope order: 'rating DESC'
 
 	private
 	def set_rating_to_zero
